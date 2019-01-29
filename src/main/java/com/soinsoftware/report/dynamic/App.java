2ad24c1d98780ecfd9c167feeb40874c48380cd8
Carlos Rodriguez <crodriguez@signal.co>
2019-01-29 17:03:49 -0500

    Initial commit

---
+++ src/main/java/com/soinsoftware/report/dynamic/App.java
@@ -0,0 +1,13 @@
+package com.soinsoftware.report.dynamic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
